b89d69157006b2f81a47946314d0cac1180f9bc8
kush2021 <kushpadalia2002@gmail.com>
2022-01-15 15:01:12 -0700

    Added a basic home screen template.

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,12 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
     return (
-        <SafeAreaView>
-            <Text>This is the home screen</Text>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.font}>Today's Medications</Text>
         </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FF6961",
+        alignItems: "center",
+        paddingTop: 30
+    },
+    font: {
+        color: "white",
+        fontSize: 36,
+        fontWeight: "bold"
+    }
+});
 
 export default Home;
