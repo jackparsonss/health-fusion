2caf1ad7eafe5698c94c65320a077d83d5b39b04
Jack <jackpar1113@gmail.com>
2022-01-15 14:31:59 -0700

    add icons to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,27 +8,33 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import { Icon } from "react-native-elements";
 
 const data = [
     {
         id: 1,
         screen: "Home",
+        icon: "home",
     },
     {
         id: 2,
         screen: "Calendar",
+        icon: "calendar",
     },
     {
         id: 3,
         screen: "Plus",
+        icon: "plus",
     },
     {
         id: 4,
         screen: "Map",
+        icon: "location-pin",
     },
     {
         id: 5,
         screen: "Settings",
+        icon: "settings",
     },
 ];
 
@@ -41,6 +47,11 @@ const Footer = () => {
             onPress={() => navigation.navigate(item.screen)}
         >
             <View>
+                <Icon
+                    type="simple-line-icon"
+                    color="#ABAFBA"
+                    name={item.icon}
+                />
                 <Text style={styles.txt}>{item.screen}</Text>
             </View>
         </TouchableOpacity>
