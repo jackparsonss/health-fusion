604caf0d4945d917536d3600597a8d4c2f2ef6a3
kush2021 <kushpadalia2002@gmail.com>
2022-01-15 17:19:02 -0700

    Added an agenda template onto the calendar screen.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts, Inter_200ExtraLight } from "@expo-google-fonts/inter";
 import Footer from "./src/Components/Footer";
 import Calendar from "./src/Screens/Calendar";
 import Home from "./src/Screens/Home";
@@ -9,6 +10,10 @@ import Settings from "./src/Screens/Settings";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+    let [fontsLoaded] = useFonts({
+        Inter_200ExtraLight,
+    });
+
     return (
         <NavigationContainer>
             <Stack.Navigator>
--- src/Screens/Calendar.js
+++ src/Screens/Calendar.js
@@ -1,12 +1,61 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { Calendar, CalendarList, Agenda, LocaleConfig } from "react-native-calendars";
 
-const Calendar = () => {
+LocaleConfig.locales["default"] = {
+    monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    dayNamesShort: ["S", "M", "T", "W", "T", "F", "S"],
+    today: ["Today"]
+};
+LocaleConfig.defaultLocale = "default";
+
+const CalendarPage = () => {
+
     return (
-        <SafeAreaView>
-            <Text>This is the calendar screen</Text>
+        <SafeAreaView style={styles.container}>
+            <Agenda
+                items={{
+                    "2022-01-15": [{ name: "Aspirin" }],
+                    "2022-01-16": [{ name: "Cough Syrup", height: 80 }],
+                }}
+                theme={{
+                    backgroundColor: "#FF6961",
+                    calendarBackground: "#ffffff",
+                    textSectionTitleColor: "#ABAFBA",
+                    textSectionTitleDisabledColor: "#ABAFBA",
+                    selectedDayBackgroundColor: "#04C38E",
+                    selectedDayTextColor: "#ffffff",
+                    dayTextColor: "#000",
+                    textDisabledColor: "#ABAFBA",
+                    dotColor: "#04C38E",
+                    selectedDotColor: "#FF6961",
+                    monthTextColor: "#000",
+                    textDayFontFamily: "Inter_200ExtraLight",
+                    textMonthFontFamily: "Inter_200ExtraLight",
+                    textDayHeaderFontFamily: "Inter_200ExtraLight",
+                    textDayFontWeight: "200",
+                    textMonthFontWeight: "bold",
+                    textDayHeaderFontWeight: "200",
+                    textDayFontSize: 16,
+                    textMonthFontSize: 16,
+                    textDayHeaderFontSize: 16,
+                    agendaDayTextColor: "white",
+                    agendaDayNumColor: "white",
+                    agendaTodayColor: "#04C38E",
+                    agendaKnobColor: "#04C38E",
+                }}
+            />
         </SafeAreaView>
     );
 };
 
-export default Calendar;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FF6961",
+    }
+})
+
+export default CalendarPage;
