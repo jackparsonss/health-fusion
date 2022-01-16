407df63a15b906943eb1176e63b0acf67c738f92
Jack <jackpar1113@gmail.com>
2022-01-15 22:20:35 -0700

    add setting page with logout functionality

--- src/Screens/Settings.js
+++ src/Screens/Settings.js
@@ -1,12 +1,55 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { View, Text } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useNavigation } from "@react-navigation/native";
 
 const Settings = () => {
+    const navigation = useNavigation();
+    const handleLogout = async () => {
+        await signOut(auth);
+        navigation.navigate("Login");
+    };
+
     return (
-        <SafeAreaView>
-            <Text>This is the settings screen</Text>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.buttonContainer}>
+                <Text style={styles.txt}>Settings</Text>
+                <TouchableOpacity style={styles.button} onPress={handleLogout}>
+                    <Text style={styles.buttonText}>Logout</Text>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     );
 };
 
 export default Settings;
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+    },
+    txt: {
+        fontWeight: "bold",
+        fontSize: 40,
+    },
+    buttonContainer: {
+        width: "80%",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 40,
+    },
+    button: {
+        marginTop: 525,
+        backgroundColor: "#FF6961",
+        width: "80%",
+        padding: 15,
+        borderRadius: 10,
+        alignItems: "center",
+    },
+    buttonText: {
+        color: "white",
+        fontWeight: "700",
+        fontSize: 18,
+    },
+});
